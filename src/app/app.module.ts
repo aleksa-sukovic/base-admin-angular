2d5ff47cee7cd832282a59512938d99dda80ed77
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-03-23 16:31:53 +0100

    Refactors routing to support multiple module setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { ThemeModule } from './@theme/theme.module';
+import { AppRoute } from './app.routing';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'corporate' }),
-    ThemeModule.forRoot(),
-    NbLayoutModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        AppRoute,
+        BrowserModule,
+        BrowserAnimationsModule,
+        NbThemeModule.forRoot({ name: 'corporate' }),
+        ThemeModule.forRoot(),
+        NbLayoutModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,6 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [];
+
+export const AppRoute: ModuleWithProviders = RouterModule.forRoot(routes);
