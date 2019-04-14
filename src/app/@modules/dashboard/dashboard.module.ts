f95932064e5105cb16b9488cfd0c88b35df6fc10
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-04-14 18:00:30 +0200

    Updates Dashboard component

--- src/app/@modules/dashboard/dashboard.module.ts
+++ src/app/@modules/dashboard/dashboard.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { DashboardRoute } from './dashboard.routing';
 import { Dashboard } from './components/dashboard/dashboard.component';
+import { ThemeModule } from 'src/app/@theme/theme.module';
 
 @NgModule({
     declarations: [
         Dashboard
     ],
     imports: [
-        DashboardRoute
+        DashboardRoute,
+        ThemeModule
     ],
     providers: []
 })
