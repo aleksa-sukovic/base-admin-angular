7bcc05dd825c355716ec9f205fa04b157a2c43da
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-04-21 17:46:51 +0200

    Hides sidebar for users which are not logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,28 @@ import { Component, ViewChild, OnDestroy } from '@angular/core';
 import { SidebarComponent } from './@shared/components/sidebar/sidebar.component';
 import { LocaleService } from './@modules/locale/services/locale.service';
 import { LanguagePickerComponent } from './@shared/components/language-picker/language-picker.component';
+import { AuthService } from './@modules/auth/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent
+export class AppComponent implements OnDestroy
 {
     @ViewChild(SidebarComponent) protected sidebarComponent: SidebarComponent;
+    protected loggedInSubscription: Subscription;
+    protected isLoggedIn: boolean;
 
-    constructor(private localeService: LocaleService)
+    constructor(private authService: AuthService)
     {
-        //
+        this.loggedInSubscription = AuthService.isLoggedInObservable.subscribe(value => this.isLoggedIn = value);
+    }
+
+    public ngOnDestroy(): void
+    {
+        this.loggedInSubscription.unsubscribe();
     }
 
     protected onToggleSidebar(): void
